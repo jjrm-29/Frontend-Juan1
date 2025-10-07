b2ea293e4c05b8011db12ed560fe70609f1dba5c
jjrm-29 <juanjosereyesm16@gmail.com>
2025-10-07 11:40:22 -0600

    Estructura inicial del proyecto frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"; 
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+       <h1> Veterinaria Juan Martinez </h1>  
+        </>
+  );
+}
+
+export default App;
