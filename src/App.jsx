1bf605b63b472ea778f1071c2009b3eeb3bfb911
jjrm-29 <juanjosereyesm16@gmail.com>
2025-10-09 09:23:37 -0600

    Estructura de titulo y mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"; 
 import './App.css';
+import Titulo from './components/Titulo.jsx';
+import Mensaje from './components/Mensaje.jsx';
 
 const App = () => {
   return (
     <>
-       <h1> Veterinaria Juan Martinez </h1>  
-        </>
+     <h1> Veterinaria Juan Martinez  </h1> 
+     <Titulo /> 
+
+    </>
   );
 }
 
